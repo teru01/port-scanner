ec047060a7f50a6d10ee2450a5c5292e937ca7ae
teruya <abas.trace.on@gmail.com>
2019-05-08 21:19:00 +0900

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate rayon;
 
-use std::{ net, env, thread, time, fs, collections };
-use pnet::packet::{ tcp, ip };
+use pnet::packet::{ip, tcp};
 use pnet::transport::{self, TransportProtocol};
+use std::{collections, env, fs, net, thread, time};
+#[macro_use]
+extern crate log;
 
 const TCP_SIZE: usize = 20;
 const MAXIMUM_PORT_NUM: u16 = 1023;
@@ -16,16 +18,17 @@ struct PacketInfo {
 
 #[derive(Copy, Clone)]
 enum ScanType {
-    SynScan = tcp::TcpFlags::SYN as isize,
-    FinScan = tcp::TcpFlags::FIN as isize,
-    XmasScan = tcp::TcpFlags::FIN as isize | tcp::TcpFlags::URG as isize | tcp::TcpFlags::PSH as isize,
-    NullScan = 0
+    Syn = tcp::TcpFlags::SYN as isize,
+    Fin = tcp::TcpFlags::FIN as isize,
+    Xmas =
+        tcp::TcpFlags::FIN as isize | tcp::TcpFlags::URG as isize | tcp::TcpFlags::PSH as isize,
+    Null = 0,
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 3 {
-        eprintln!("Bad nunber of arguemnts");
+        error!("Bad nunber of arguemnts");
         std::process::exit(1);
     }
 
@@ -34,80 +37,119 @@ fn main() {
         let lines: Vec<_> = contents.split('\n').collect();
         let mut map = collections::HashMap::new();
         for line in lines {
-            let elm: Vec<_> = line.split('=').map(|s| s.trim()).collect();
+            let elm: Vec<_> = line.split('=').map(str::trim).collect();
             if elm.len() == 2 {
                 map.insert(elm[0], elm[1]);
             }
         }
         PacketInfo {
-            my_ipaddr:       map.get("MY_IPADDR").expect("missing my_ipaddr").parse().expect("invalid ipaddr"),
-            target_ipaddr:   "0.0.0.0".parse().unwrap(),
-            my_port:         map.get("MY_PORT").expect("missing my_port").parse().expect("invalid port number"),
-            scan_type:       ScanType::SynScan
+            my_ipaddr: map
+                .get("MY_IPADDR")
+                .expect("missing my_ipaddr")
+                .parse()
+                .expect("invalid ipaddr"),
+            target_ipaddr: "0.0.0.0".parse().unwrap(),
+            my_port: map
+                .get("MY_PORT")
+                .expect("missing my_port")
+                .parse()
+                .expect("invalid port number"),
+            scan_type: ScanType::Syn,
         }
     };
 
     packet_info.target_ipaddr = args[1].parse().expect("invalid target ipaddr");
     packet_info.scan_type = match args[2].as_str() {
-        "sS" => ScanType::SynScan,
-        "sF" => ScanType::FinScan,
-        "sX" => ScanType::XmasScan,
-        "sN" => ScanType::NullScan,
-        _    => panic!("Undefined scan method")
+        "sS" => ScanType::Syn,
+        "sF" => ScanType::Fin,
+        "sX" => ScanType::Xmas,
+        "sN" => ScanType::Null,
+        _ => panic!("Undefined scan method"),
     };
 
-    let (mut ts, mut tr) = transport::transport_channel(1024, transport::TransportChannelType::Layer4(TransportProtocol::Ipv4(ip::IpNextHeaderProtocols::Tcp))).unwrap();
+    let (mut ts, mut tr) = transport::transport_channel(
+        1024,
+        transport::TransportChannelType::Layer4(TransportProtocol::Ipv4(
+            ip::IpNextHeaderProtocols::Tcp,
+        )),
+    )
+    .unwrap();
 
-    rayon::join(|| send_packet(&mut ts, &packet_info),
-                || receive_packets(&mut tr, &packet_info)
+    rayon::join(
+        || send_packet(&mut ts, &packet_info),
+        || receive_packets(&mut tr, &packet_info),
     );
 }
 
-// 指定のレンジにパケットを送信
-fn send_packet(ts: &mut transport::TransportSender, packet_info: &PacketInfo) {
+/**
+ * 指定のレンジにパケットを送信
+ */
+fn send_packet(
+    ts: &mut transport::TransportSender,
+    packet_info: &PacketInfo,
+) -> Result<(), failure::Error> {
     let mut packet = build_packet(packet_info);
-    for i in 1..MAXIMUM_PORT_NUM+1 {
-        let mut tcp_header = tcp::MutableTcpPacket::new(&mut packet).unwrap();
+    for i in 1..=MAXIMUM_PORT_NUM {
+        let mut tcp_header =
+            tcp::MutableTcpPacket::new(&mut packet).ok_or_else(|| failure::err_msg("invalid packet"))?;
         reregister_destination_port(i, &mut tcp_header, packet_info);
         thread::sleep(time::Duration::from_millis(5));
-        ts.send_to(tcp_header, net::IpAddr::V4(packet_info.target_ipaddr)).expect("failed to send");
+        ts.send_to(tcp_header, net::IpAddr::V4(packet_info.target_ipaddr))?;
     }
+    Ok(())
 }
 
-// TCPヘッダの宛先ポート情報を書き換える
-fn reregister_destination_port(target: u16, tcp_header: &mut tcp::MutableTcpPacket, packet_info: &PacketInfo) {
+/**
+ * TCPヘッダの宛先ポート情報を書き換える。
+ * チェックサムを計算し直す必要がある。
+ */
+fn reregister_destination_port(
+    target: u16,
+    tcp_header: &mut tcp::MutableTcpPacket,
+    packet_info: &PacketInfo,
+) {
     tcp_header.set_destination(target);
-    let checksum = tcp::ipv4_checksum(&tcp_header.to_immutable(), &packet_info.my_ipaddr, &packet_info.target_ipaddr);
+    let checksum = tcp::ipv4_checksum(
+        &tcp_header.to_immutable(),
+        &packet_info.my_ipaddr,
+        &packet_info.target_ipaddr,
+    );
     tcp_header.set_checksum(checksum);
 }
 
-// パケットを受信してスキャン結果を出力する。
-fn receive_packets(tr: &mut transport::TransportReceiver, packet_info: &PacketInfo) {
+/**
+ * パケットを受信してスキャン結果を出力する。
+ */
+fn receive_packets(
+    tr: &mut transport::TransportReceiver,
+    packet_info: &PacketInfo,
+) -> Result<(), failure::Error> {
     let mut reply_ports = Vec::new();
     let mut packet_iter = transport::tcp_packet_iter(tr);
     loop {
         // ターゲットからの返信パケット
         let tcp_packet = match packet_iter.next() {
             Ok((tcp_packet, _)) => {
-                if tcp_packet.get_destination() != packet_info.my_port {
+                if tcp_packet.get_destination() == packet_info.my_port {
+                    tcp_packet
+                } else {
                     continue;
                 }
-                tcp_packet
             }
-            Err(_) => continue
+            Err(_) => continue,
         };
-        
+
         let target_port = tcp_packet.get_source();
         match packet_info.scan_type {
-            ScanType::SynScan => {
+            ScanType::Syn => {
                 if tcp_packet.get_flags() == tcp::TcpFlags::SYN | tcp::TcpFlags::ACK {
                     println!("port {} is open", target_port);
                 }
-            },
-            // SYNスキャン以外は返答が返ってきたポート（＝閉じているポート）を記録
-            ScanType::FinScan | ScanType::XmasScan | ScanType::NullScan => {
+            }
+            // SYNスキャン以外はレスポンスが返ってきたポート（＝閉じているポート）を記録
+            ScanType::Fin | ScanType::Xmas | ScanType::Null => {
                 reply_ports.push(target_port);
-            },
+            }
         }
 
         // 手抜き：スキャン対象の最後のポートに対する返信が帰ってこれば終了
@@ -115,32 +157,35 @@ fn receive_packets(tr: &mut transport::TransportReceiver, packet_info: &PacketIn
             continue;
         }
         match packet_info.scan_type {
-            ScanType::FinScan | ScanType::XmasScan | ScanType::NullScan => {
-                for i in 1..MAXIMUM_PORT_NUM+1 {
-                    match reply_ports.iter().find(|&&x| x == i) {
-                        None => {
-                            println!("port {} is open", i);
-                        },
-                        _ => {}
+            ScanType::Fin | ScanType::Xmas | ScanType::Null => {
+                for i in 1..=MAXIMUM_PORT_NUM {
+                    if reply_ports.iter().find(|&&x| x == i).is_none() {
+                        println!("port {} is open", i);
                     }
                 }
-            },
+            }
             _ => {}
         }
-        return;
+        return Ok(());
     }
 }
 
 // パケットを生成する。
-fn build_packet(packet_info: &PacketInfo) -> [u8; TCP_SIZE]{
+fn build_packet(packet_info: &PacketInfo) -> [u8; TCP_SIZE] {
     // TCPヘッダの作成
     let mut tcp_buffer = [0u8; TCP_SIZE];
     let mut tcp_header = tcp::MutableTcpPacket::new(&mut tcp_buffer[..]).unwrap();
     tcp_header.set_source(packet_info.my_port);
+
+    // オプションを含まないので、20オクテットまでがTCPヘッダ。4オクテット単位で指定する
     tcp_header.set_data_offset(5);
     tcp_header.set_flags(packet_info.scan_type as u16);
-    let checksum = tcp::ipv4_checksum(&tcp_header.to_immutable(), &packet_info.my_ipaddr, &packet_info.target_ipaddr);
+    let checksum = tcp::ipv4_checksum(
+        &tcp_header.to_immutable(),
+        &packet_info.my_ipaddr,
+        &packet_info.target_ipaddr,
+    );
     tcp_header.set_checksum(checksum);
 
-    return tcp_buffer;
+    tcp_buffer
 }
